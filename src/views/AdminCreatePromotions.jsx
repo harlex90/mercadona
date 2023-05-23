fa7879f45c5c4312def70044e4a64b6374f81c9f
harlex90 <soraya.gaillard@hotmail.fr>
2023-05-23 14:59:04 +0200

    finish date implementation in admin promo

--- src/views/AdminCreatePromotions.jsx
+++ src/views/AdminCreatePromotions.jsx
@@ -6,10 +6,12 @@ import Entete from "./Entete";
 
 const AdminCreatePromos = () => {
     const { products } = useProducts();
-
+    
     const [inputDiscount, setInputDiscount] = React.useState(0);
     const [inputProduct, setInputProduct] = React.useState();
-
+    const [inputStart, setInputStart] = React.useState();
+    const [inputEnd, setInputEnd] = React.useState();
+    
     const navigate = useNavigate();
     const onSubmit = () => {
         if(!inputProduct) {
@@ -19,23 +21,26 @@ const AdminCreatePromos = () => {
         axios.post('/promotions', {
             product_id: inputProduct,
             discount: inputDiscount,
-            start_date: 0,
-            end_date: 0,
+            start_date: parseInt(new Date(inputStart).getTime() / 1000), // TODO remove / 1000 after backend fix int type
+            end_date: parseInt(new Date(inputEnd).getTime() / 1000), // TODO remove / 1000 after backend fix int type
         }).then(() => {
             navigate('/admin/promos')
         })
     }
+
     return (
         <div>
             <Entete />
             <p>Create a new promo</p>
-            <select value={inputProduct} onChange={(e) => setInputProduct(e.currentTarget.value)}>
+            <select value={inputProduct} onChange={(e) => setInputProduct(parseInt(e.currentTarget.value))}>
                 <option value={null}> -- select an option -- </option>
                 {products.map((product) => (
                     <option key={product.id} value={product.id} label={product.name} />
                 ))}
             </select>
             <input type="number" value={inputDiscount} min={0} max={1} step={0.05} onChange={(e) => setInputDiscount(parseFloat(e.currentTarget.value))} placeholder="discount of the product"/>
+            <input type="date" value={inputStart} onChange={(e) => setInputStart(e.currentTarget.value)} />
+            <input type="date" value={inputEnd} onChange={(e) => setInputEnd(e.currentTarget.value)} />
             <button onClick={onSubmit}>Submit</button>
         </div>
     )
