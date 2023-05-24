e2fc11f787ece92058577f84218de690dd16d473
harlex90 <soraya.gaillard@hotmail.fr>
2023-05-24 15:54:34 +0200

    creation of page AdminDeleteProduct

---
+++ src/views/AdminDeleteProduct.jsx
@@ -0,0 +1,7 @@
+import axios from "axios";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useProducts from "../hooks/useProducts";
+import Entete from "./Entete";
+
+
