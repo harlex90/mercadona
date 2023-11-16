3e00c0679784caafd7b9dcc2337d891f680b1ebb
harlex90 <soraya.gaillard@hotmail.fr>
2023-11-16 19:54:03 +0100

    Display categories list and buttons

--- src/components/AdminCategoryButtons.jsx
+++ src/components/AdminCategoryButtons.jsx
@@ -1,6 +1,8 @@
 import { useIsAuthenticated } from "react-auth-kit";
 import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 import useCategories from "../hooks/useCategories";
+import ItemCategory from "./ItemCategory";
 
 
 const AdminCategoryButtons = () => {
@@ -18,24 +20,21 @@ const AdminCategoryButtons = () => {
     }
 
     return(
-        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-            {isAuthenticated() && (
-            <h3>
-            CATEGORIES
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "5%", marginBottom: "5%", width: "100%"}}>
+          {isAuthenticated() && (
+            <h3 style={{display: "flex", marginLeft: "10%", gap: "100px", width: "100%"}}>
+              CATEGORIES
             </h3>)}
-        <div style={{display: "flex", justifyContent: "center", width: "100%", alignItems: "center", gap: "50px"}}>
-            {isAuthenticated() && <button type='button' style={{display:"flex", justifyContent: "center", alignItems: "center", fontSize: "80px", backgroundColor: "white", width: "80px", height: "80px", borderRadius: "50%", border: "2px solid black"}}><a href="#" onClick={onPlus} style={{textDecoration: "none"}}>+</a></button>}
-            {isAuthenticated() && <button type='button' style={{display:"flex", justifyContent: "center", alignItems: "center", fontSize: "80px", backgroundColor: "white", width: "80px", height: "80px", borderRadius: "50%", border: "2px solid black"}}><a href="#" onClick={onMinus} style={{textDecoration: "none"}}>-</a></button>}
-            
-            
-            
-        </div>
-        {categories.map((category) => (
-        <div key={category.id} style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-            <p style={{ margin: "0px" }}>id: {category.id}</p>
-            <Link to={`/admin/categories/${category.id}`}>name: {category.name}</Link>
-        </div>
-        ))}
+            <div style={{display: "flex", marginLeft: "10%", gap: "100px", width: "100%"}}>
+              <div style={{display: "flex", gap: "80px"}}>
+                {categories.map((category) => <ItemCategory key={category.id} category={category} />)}
+              </div>
+              <div style={{display: "flex", alignItems: "center", gap: "50px"}}>
+                {isAuthenticated() && <button type='button' style={{display:"flex", justifyContent: "center", alignItems: "center", fontSize: "80px", backgroundColor: "white", width: "80px", height: "80px", borderRadius: "50%", border: "2px solid black"}}><a href="#" onClick={onPlus} style={{textDecoration: "none"}}>+</a></button>}
+                {isAuthenticated() && <button type='button' style={{display:"flex", justifyContent: "center", alignItems: "center", fontSize: "80px", backgroundColor: "white", width: "80px", height: "80px", borderRadius: "50%", border: "2px solid black"}}><a href="#" onClick={onMinus} style={{textDecoration: "none"}}>-</a></button>}
+              </div>
+            </div>
+        
         </div>
 
     )
