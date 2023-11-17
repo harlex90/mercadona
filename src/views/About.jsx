4e30daab0403aca4149877ed9f99a6ccbb0c952d
harlex90 <soraya.gaillard@hotmail.fr>
2023-11-17 13:04:56 +0100

    Modifications on About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -19,9 +19,7 @@ const About = () => {
              <p>Forte de ses 1675 établissements répartis sur le territoire espagnol, Mercadona s'impose comme un pilier majeur dans le domaine du retail. Avec un chiffre d'affaires spectaculaire de 20 milliards d'euros, notre entreprise illustre son succès à travers une politique de prix compétitifs et des promotions régulières. Ainsi, nous nous assurons que nos clients peuvent toujours trouver une sélection d'au moins une centaine de produits en promotion, tout au long de l'année.</p>
 
 <p>Toutefois, conscient de l'importance grandissante de la dimension écologique dans notre secteur, Mercadona a décidé de marquer un tournant dans sa stratégie de communication. Jusqu'à présent, notre communication a principalement reposé sur l'utilisation de tracts publicitaires. 
-    Cependant, dans un effort pour économiser les ressources et minimiser notre impact environnemental, nous envisageons désormais une transition vers le numérique.
-
-Ainsi, nous sommes actuellement en train de développer une application web innovante, qui permettra à nos clients d'accéder à nos promotions et de découvrir nos produits à partir de leurs appareils connectés. Ce changement traduit notre engagement à adopter des pratiques plus durables, tout en améliorant l'expérience de nos clients.</p>
+    Cependant, dans un effort pour économiser les ressources et minimiser notre impact environnemental, nous avons fait le choix de vous proposer cette applicationen web innovante, qui vous permet d'accéder à nos promotions et de découvrir nos produits à partir de vos appareils connectés. Ce changement traduit notre engagement à adopter des pratiques plus durables, tout en améliorant votre expérience client.</p>
             </div>
             <Footer />
         </div>
